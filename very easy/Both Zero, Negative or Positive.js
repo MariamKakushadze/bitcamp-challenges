c2b1dea42fb64d0c5e5902facc000e2f67e4be22
MariamKakushadze <102153226+MariamKakushadze@users.noreply.github.com>
2022-07-22 14:55:29 +0400

    Create Both Zero, Negative or Positive.js

---
+++ very easy/Both Zero, Negative or Positive.js
@@ -0,0 +1,11 @@
+function both(x,y){
+    if(x>0 && y>0){
+        return true;
+    }else if(x==0 && y==0){
+        return true;
+    }else if(x<0 && y<0){
+        return true;
+    }else{
+        return false;
+    }
+}
